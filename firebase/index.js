281006c3565ab567985de6d65d0f89b7a8b063bd
Rishav-e <200100131@iitb.ac.in>
2022-05-29 12:00:09 +0530

    first commit

---
+++ firebase/index.js
@@ -0,0 +1,10 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ecommerce-a1387.firebaseapp.com",
+});
+
+module.exports = admin;
